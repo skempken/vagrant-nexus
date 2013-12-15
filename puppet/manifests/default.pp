e8cbab4766c21797fc6a50cb7690a9916ad1e6ba
Sebastian Kempken <mail@sebastian-kempken.de>
2013-12-15 13:39:19 +0100

    Initial setup

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,31 @@
+  # puppetlabs-java
+  # NOTE: Nexus requires
+  class{ '::java':
+    distribution => 'jdk'
+  }
+
+  group{ 'nexus':
+    ensure => present,
+    system => true
+  }
+
+  user{ 'nexus':
+    ensure => present,
+    comment => 'Nexus user',
+    gid     => 'nexus',
+    home    => '/srv/nexus',
+    shell   => '/bin/bash', # unfortunately required to start application via script.
+    system  => true,
+    require => Group['nexus']
+  }
+
+  class{ '::nexus':
+    version        => '2.7.0',
+    revision       => '05',
+    nexus_user     => 'nexus',
+    nexus_group    => 'nexus',
+    nexus_root     => '/srv' # All directories and files will be relative to this
+  }
+
+  Class['::java'] ->
+  Class['::nexus']
